5d83d7663ef858d7c55d4028375de075083145b1
rasmee da-oh <rasmee@bukitbunga.com>
2019-12-03 07:02:14 +0800

    initial commit

---
+++ forex.ps1
@@ -0,0 +1,11 @@
+﻿
+gps forex* | kill
+
+$env:FOREX_ENV="Prod"
+$env:FOREX_ENVIRONMENT="Prod"
+$env:FOREX_Branch="002"
+$env:FOREX_Terminal="002-001"
+$env:FOREX_BaseUrl="https://web-forex.azurewebsites.net/"
+
+
+.\forex\forex.exe
