7b93cfbd67e102beb69162852d3cd601f888a53b
erymuzuan <erymuzuan@hotmail.com>
2022-04-10 14:48:08 +0800

    new printer

---
+++ start-forex-printer.ps1
@@ -0,0 +1,28 @@
+﻿Param(
+    [string]$username = "",
+    [string]$password = "",
+    [string]$branch = "002",
+    [string]$terminal = "002-001",
+    [switch]$skipUpdate
+)
+
+gps forex* | kill
+
+if($skipUpdate.IsPresent -eq $false){
+
+    git fetch origin --prune
+    git merge origin/master
+
+    git lfs fetch
+    git lfs checkout
+}
+
+
+$env:FOREX_ENV="Prod"
+$env:FOREX_ENVIRONMENT="Prod"
+$env:FOREX_Branch=$branch
+$env:FOREX_Terminal=$terminal
+$env:FOREX_BaseUrl="https://web-forex.azurewebsites.net/"
+
+
+.\forex.printer\wpf.receipt.printer.exe
